d6768ab98ab55be6105e02497341b41bebb62a42
Hillel Buchsbaum <29700271+Hzbbaum@users.noreply.github.com>
2020-06-30 11:16:43 +0300

    Updates

---
+++ precache-manifest.36b837693506aef159cbc9eb19c528bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed5260fba1c2406bd4a61436d0ec850",
+    "url": "/myWay-client/index.html"
+  },
+  {
+    "revision": "03ebc63352fef231f7c1",
+    "url": "/myWay-client/static/css/main.df61099b.chunk.css"
+  },
+  {
+    "revision": "8a09a3cfe830c454deb6",
+    "url": "/myWay-client/static/js/2.00dc829e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/myWay-client/static/js/2.00dc829e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03ebc63352fef231f7c1",
+    "url": "/myWay-client/static/js/main.a7d1dcb7.chunk.js"
+  },
+  {
+    "revision": "82c645eefd271bd85132",
+    "url": "/myWay-client/static/js/runtime-main.d1f64abe.js"
+  },
+  {
+    "revision": "995e34c49e0ebcaf4561e41038bfcfe5",
+    "url": "/myWay-client/static/media/anotherbeach.995e34c4.jpg"
+  }
+]);
